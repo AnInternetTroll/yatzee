0bcf85238a7679b2e46213fd4bc522ce3212bb96
swifuCS <xlrgameryt04d@gmail.com>
2022-05-24 21:50:32 +0200

    Many new Things

--- yahtzeeTry/game.js
+++ yahtzeeTry/game.js
@@ -1,42 +1,166 @@
 export class Dice {
-  locked = false;
-  value = Math.ceil(Math.random() * 6);
+  locked;
+  value;
+
+  constructor({ value = Dice.getRandomDiceRoll(), locked = false } = {}) {
+    this.value = value;
+    this.locked = locked;
+  }
+
   roll() {
-    this.value = Math.ceil(Math.random() * 6);
+    this.value = Dice.getRandomDiceRoll();
+  }
+
+  static getRandomDiceRoll() {
+    return Math.ceil(Math.random() * 6);
+  }
+
+  toJSON() {
+    return {
+      value: this.value,
+      locked: this.locked,
+    };
+  }
+}
+
+class Objective {
+  name;
+  display;
+  locked;
+  points;
+
+  constructor({ name, display, locked = false, points = 0 } = {}) {
+    this.name = name;
+    this.locked = locked;
+    this.points = points;
+    this.display = display;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      locked: this.locked,
+      points: this.points,
+    };
   }
 }
 
 export class Game {
-  rounds = 9;
-  currentRound = 1;
-  maxRolls = 3;
-  diceRolled = 0;
-  dice = [];
-  score = 0;
+  rounds;
+  currentRound;
+  maxRolls;
+  diceRolled;
+  dice;
+  objectives;
+  #score;
 
-  constructor() {
-    for (let i = 0; i < 6; i++) {
-      this.dice.push(new Dice());
+  /**
+   * Restore a previous game from an exported instance
+   * Or set up a new game
+   */
+  constructor({
+    rounds,
+    currentRound,
+    maxRolls,
+    diceRolled,
+    dice,
+    score,
+    objectives,
+  } = {}) {
+    // If we recieve dice and there are dice then use those
+    // Else generate new dice
+    // Is it dice or die?
+    if (dice && Array.isArray(dice) && dice.length) {
+      if (typeof dice.roll === "function") this.dice = dice;
+      else this.dice = dice.map((dice) => new Dice(dice));    } else {
+      this.newDice();
     }
+
+    if (objectives && Array.isArray(objectives) && objectives.length) {
+      this.objectives = objectives.map((objective) => new Objective(objective));
+    } else {
+      this.objectives = [
+        new Objective({ name: "checkOnes", display: "Enere" }),
+        new Objective({ name: "checkTwos", display: "Toere" }),
+        new Objective({ name: "checkThrees", display: "Trere" }),
+        new Objective({ name: "checkFours", display: "Firere" }),
+        new Objective({ name: "checkFives", display: "Femere" }),
+        new Objective({ name: "checkSixes", display: "Seksere"}),
+        new Objective({ name: "checkThreeRepeats", display: "4 like" }),
+        new Objective({ name: "checkFourRepeats", display : "5 like"}),
+        new Objective({ name: "checkYahtzee", display : "Yahtzee"}),,
+      ];
+    }
+
+    // Set default variables
+    this.rounds = rounds || 9;
+    this.currentRound = currentRound || 1;
+    this.maxRolls = maxRolls || 3;
+    this.diceRolled = diceRolled || 0;
+    this.#score = score || 0;
+    this.onScoreUpdate();
   }
 
+  /**
+   * Export the data as a json string
+   * Used to save a game
+   */
+  toJSON() {
+    return {
+      rounds: this.rounds,
+      currentRound: this.currentRound,
+      maxRolls: this.maxRolls,
+      diceRolled: this.diceRolled,
+      dice: this.dice,
+      score: this.score,
+    };
+  }
+
+  /**
+   * A hook called on each dice roll
+   * Can be used to implement render logic
+   */
   onDiceRoll() {}
+  onScoreUpdate() {}
+
+  set score(value) {
+    this.#score = value;
+    this.onScoreUpdate();
+  }
+
+  get score() {
+    return this.#score;
+  }
 
   round() {
     if (this.rounds == 0) {
       return; //End game
     }
-
+    this.dice = [];
     this.throwDice();
     this.rounds--;
     this.maxRolls = 3;
   }
 
+  newDice() {
+    if (!this.dice) this.dice = [];
+    for (let i = 0; i < 5; ++i) {
+      this.dice.push(new Dice());
+    }
+    this.onDiceRoll();
+  }
+
+  /**
+   * Lock a dice
+   * Locked dice won't be re-rolled on `throwDice`
+   */
   lockDice(index) {
     this.dice[index].locked = true;
   }
 
   throwDice() {
+    if (!this.dice.length) return this.newDice();
+
     if (this.diceRolled == this.maxRolls) {
       return; // cant throw the dice anymore
     } else {
@@ -48,29 +172,58 @@ export class Game {
     }
     this.onDiceRoll();
   }
-  checkN(n) {
-    return duplicatesCount(this.dice)[n] * n;
+  /**
+   * Private helper method
+   * Used to calculate the score
+   * Of dice sums of the same number
+   * @param {number} n The dice value to be checked against
+   *
+   * @example
+   * ```js
+   * dice = [1,2,2,2,2];
+   * checkN(2) == 8
+   * ```
+   */
+  #checkN(n) {
+    return (this.duplicatesCount(this.dice)[n] || 0) * n;
   }
 
   checkOnes() {
-    return checkN(1);
+    return this.#checkN(1);
   }
   checkTwos() {
-    return checkN(2);
+    return this.#checkN(2);
   }
   checkThrees() {
-    return checkN(3);
+    return this.#checkN(3);
   }
   checkFours() {
-    return checkN(4);
+    return this.#checkN(4);
   }
   checkFives() {
-    return checkN(5);
+    return this.#checkN(5);
   }
   checkSixes() {
-    return checkN(6);
+    return this.#checkN(6);
   }
 
+  /**
+   * Generate a frequency table from a dice array
+   * @param {Dice[]} dice
+   *
+   * @returns {Record<string, number>} The frequency table
+   * @example
+   * ```js
+   * const dice = [1,1,1,2,4,5];
+   * console.log(duplicatesCount(dice))
+   * {
+   *  "1": 3,
+   *  "2": 1,
+   *  "4": 1,
+   *  "5": 1
+   * }
+   * ```
+   */
   duplicatesCount(dice) {
     const counts = {};
 
@@ -81,23 +234,28 @@ export class Game {
     return counts;
   }
 
+  addUpAllDice() {
+    let sum = 0;
+    for (let i = 0, len = this.dice.length; i < len; ++i) {
+      sum += this.dice[i].value;
+    }
+    return sum;
+  }
+
   checkThreeRepeats() {
-    const repeats = Object.entries(duplicatesCount(this.dice)).find(
-      (dice) => dice[1] >= 3
-    );
-    if (repeats) return repeats[0] * repeats[1];
+    const duplicateCount = this.duplicatesCount(this.dice);
+    const repeats = Object.entries(duplicateCount).find((dice) => dice[1] >= 3);
+    if (repeats) return this.addUpAllDice();
   }
   checkFourRepeats() {
-    const repeats = Object.entries(duplicatesCount(this.dice)).find(
-      (dice) => dice[1] >= 5
-    );
-    if (repeats) return repeats[0] * repeats[1];
+    const duplicateCount = this.duplicatesCount(this.dice);
+    const repeats = Object.entries(duplicateCount).find((dice) => dice[1] >= 4);
+    if (repeats) return this.addUpAllDice();
   }
 
   checkYahtzee() {
-    const repeats = Object.entries(duplicatesCount(this.dice)).find(
-      (dice) => dice[1] >= 5
-    );
-    if (repeats) return repeats[0] * repeats[1];
+    const duplicateCount = this.duplicatesCount(this.dice);
+    const repeats = Object.entries(duplicateCount).find((dice) => dice[1] >= 5);
+    if (repeats) return 50;
   }
 }
--- yahtzeeTry/index.js
+++ yahtzeeTry/index.js
@@ -1,26 +1,113 @@
-import { Game } from './game.js'
+// importere klassene fra game.js
+import { Game } from "./game.js";
 
+// hente DOM elementer
 const rollButton = document.getElementById("rollDiceButton");
 const dicesContainer = document.querySelector(".dices");
+const scoreTablesContainer = document.querySelector(".scoretables");
+const restartGame = document.querySelector("#restartGame");
+const nextRoundButton = document.querySelector("#nextRound");
+const totalScoreEl = document.querySelector("#total");
 
-const Yahtzee = new Game;
+const previousGame = localStorage.getItem("previousGame");
 
+restartGame.addEventListener("click", (e) => {
+  localStorage.removeItem("previousGame");
+  window.location.reload();
+});
 
+nextRoundButton.addEventListener("click", (e) => {
+  game.round();
+});
 
-rollButton.addEventListener("click", (e)=>{
-    e.preventDefault();
-    Yahtzee.throwDice()
-})
+// Starter et gam
+const game = new Game(previousGame ? JSON.parse(previousGame) : undefined);
+renderDice();
 
-Yahtzee.onDiceRoll = ()=> {
+// Kalle på throwDice() fra game.js for å kaste nytt terning
+rollButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  // funksjon fra game.js
+  game.throwDice();
+});
 
-dicesContainer.innerHTML = ""
+// Tømme diven før vi renderer et ternining
+game.onDiceRoll = () => {
+  renderDice();
+  saveGame();
+};
 
-Yahtzee.dice.forEach(dice =>{
+function renderDice() {
+  dicesContainer.innerHTML = "";
+
+  // rendere et dynamist terning for hvert roll
+  game.dice.forEach((dice, index) => {
+    // hver knapp for en dataset med sin index
     dicesContainer.innerHTML += `
-        <button>${dice.value}</button>
+        <button class="dice ${
+          dice.locked ? "lock" : ""
+        }" data-index="${index}">${dice.value}</button>
     `;
-})
+  });
+
+  // Henter de nye rendera knappene med datasettet
+  const lockDice = document.querySelectorAll("[data-index]");
+
+  lockDice.forEach((dice) => {
+    dice.addEventListener("click", (e) => {
+      // låser en terning ved hjelp av indexen til en ternign
+      game.lockDice(dice.dataset.index);
+      dice.classList.add("lock");
+    });
+  });
+  renderRoundTables();
 }
 
-Yahtzee.round()
+function renderRoundTables() {
+  scoreTablesContainer.innerHTML = "";
+  game.objectives.forEach((objective) => {
+    scoreTablesContainer.innerHTML += `
+    <tr>
+          <td>${objective.display}</td>
+          <td>
+          <button 
+            class="rolled-score" 
+            data-objective="${objective.name}"
+            data-locked="${objective.locked}"
+          >
+              ${objective.locked ? objective.points : game[objective.name]() || 0}
+          </button>
+          </td>
+        </tr>
+        `;
+  });
+  requestAnimationFrame(() => {
+    const objectivesElements = document.querySelectorAll("button[data-objective]")
+    console.debug(objectivesElements);
+    objectivesElements.forEach((objectiveEl) => {
+      objectiveEl.addEventListener("click", (e) => {
+        if (e.currentTarget.dataset.locked === "true") return;
+        const objectiveName = e.currentTarget.dataset.objective;
+        const objectiveIndex = game.objectives.findIndex((objective) => objective.name === objectiveName);
+        const earnedScore = game[objectiveName]();
+        game.score += earnedScore;
+        game.objectives[objectiveIndex].points = earnedScore;
+        game.objectives[objectiveIndex].locked = true;
+        e.currentTarget.dataset.locked = true;
+      });
+    });
+  });
+}
+
+function saveGame() {
+  localStorage.setItem("previousGame", JSON.stringify(game));
+}
+
+game.onScoreUpdate = () => {
+  totalScoreEl.textContent = game.score;
+  saveGame();
+};
+
+game.round();
+game.onScoreUpdate();
+renderRoundTables();
